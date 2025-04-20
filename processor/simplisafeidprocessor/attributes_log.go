9f0c282193e28aff85c528ae785c9c24cd796c25
Laban Eilers <laban@eilers.com>
2025-04-20 15:55:08 -0400

    revert mod weirdness

--- processor/simplisafeidprocessor/attributes_log.go
+++ processor/simplisafeidprocessor/attributes_log.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package simplisafeidprocessor // import "github.com/labaneilers/opentelemetry-collector-contrib/processor/simplisafeidprocessor"
+package simplisafeidprocessor // import "github.com/opentelemetry/opentelemetry-collector-contrib/processor/simplisafeidprocessor"
 
 import (
 	"context"
